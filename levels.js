80a0b83a298653b21f43be6bef8cdbac1c636876
dkruru <dkruru@gmail.com>
2025-06-05 13:45:05 +0900

    Separate level configuration

---
+++ levels.js
@@ -0,0 +1,12 @@
+const levels = [
+  {rows: 2, cols: 5},
+  {rows: 3, cols: 5},
+  {rows: 3, cols: 6},
+  {rows: 4, cols: 6},
+  {rows: 4, cols: 7},
+  {rows: 5, cols: 7},
+  {rows: 5, cols: 8},
+  {rows: 6, cols: 8},
+  {rows: 6, cols: 9},
+  {rows: 7, cols: 10}
+];
